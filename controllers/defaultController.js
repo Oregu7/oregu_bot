21c5df8cc6561b70a8c6bee212ac96a9173da9c4
Oregu7 <jekkicool@yandex.ru>
2018-09-26 22:59:37 +0300

    + apiai

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,3 +1,24 @@
+const apiai = require("apiai")(process.env.APIAI_TOKEN);
+
+function apiaiRequest(userId, query = "") {
+    return new Promise((resolve, reject) => {
+        let request = apiai.textRequest(query, {
+            sessionId: userId,
+        });
+
+        request.on("response", resolve);
+        request.on("error", reject);
+        request.end();
+    });
+}
+
 module.exports = async(ctx) => {
-    return ctx.send({ message: "заглушка" });
+    const { peerId: userId, text } = ctx;
+    if (!text)
+        return ctx.send({ message: "Неизвестная команда" });
+
+    const response = await apiaiRequest(userId, text);
+    const message = response.result.fulfillment.speech;
+
+    return ctx.send({ message });
 };
